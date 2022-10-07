2918b0000acf9dd0231364885e0f07db86c2b897
Ubuntu <ubuntu@ip-172-31-45-148.ap-south-1.compute.internal>
2022-10-07 02:29:30 +0000

    updated

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -8,14 +8,14 @@ terraform {
   }
 }
 
-# # Provider Block
-# provider "aws" {
-#   #profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
-#   region  = "ap-south-1"
-# }
+# Provider Block
+provider "aws" {
+  #profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "ap-south-1"
+}
 
-# # Resource Block
-# resource "aws_instance" "ec2demo" {
-#   ami           = "ami-0be2609ba883822ec" # Amazon Linux in us-east-1, update as per your region
-#   instance_type = "t2.micro"
-# }
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-0be2609ba883822ec" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t2.micro"
+}
